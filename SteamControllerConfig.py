4a19a04c2f7c068e6030c008f611d1e5137dad6b
Gregory Gluszek <ggluszek@gmail.com>
2016-10-08 16:15:24 -0400

    Reading Firmware Revision
    Added class structure to design and ability to request and parse firmware
     revision information.

--- SteamControllerConfig.py
+++ SteamControllerConfig.py
@@ -3,20 +3,71 @@
 import usb.core
 import usb.util
 
+from datetime import datetime
+
 import sys, getopt
 
-dev = None
+class SteamControllerConfig:
+	'Class for configuring of a Steam Controller'
 
-def playSong(index):
-	payload = [0xb6, 0x04, index, 0x00 , 0x00, 0x00, 0x00, 0x00 , 0x00, 0x00, 0x00, 
-		0x00 , 0x00, 0x00, 0x00, 0x00 , 0x00, 0x00, 0x00, 0x00 , 0x00, 0x00, 
-		0x00, 0x00 , 0x00, 0x00, 0x00, 0x00 , 0x00, 0x00, 0x00, 0x00]
+	usbDev = None
+	firmwareRev = 0
+	bootloaderRev = 0	
 
-	global dev
-	dev.ctrl_transfer(bmRequestType=0x21, bRequest=9, wValue=0x0300, wIndex=2, 
-		data_or_wLength=payload)
+	def __init__(self):
+		# TODO: only searches for wired controller 
+		self.usbDev = usb.core.find(idVendor=0x28DE, idProduct=0x1102)
+
+		if self.usbDev is None:
+		    raise ValueError('Wired Steam Controller not found')
+
+		print "Found Wired Steam Controller"
+
+		# TODO: what about reattaching controller on exit?
+		if (self.usbDev.is_kernel_driver_active(interface=2)):
+			usbDev.detach_kernel_driver(interface=2)
+
+		# TODO: need to claim interface?
+		#self.usbDev.claim_interface(
+
+		payload = [0x83, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 ,0x00, 0x00, 
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+		
+		# Send request for revision info
+		self.usbDev.ctrl_transfer(bmRequestType=0x21, bRequest=9, wValue=0x0300, wIndex=2, 
+			data_or_wLength=payload)
+
+		# Get revision info
+		payload = self.usbDev.ctrl_transfer(bmRequestType=0xa1, bRequest=1, wValue=0x0300, wIndex=2, 
+			data_or_wLength=64)
+
+		for index in range(26, 22, -1):
+			self.firmwareRev *= 256 
+			self.firmwareRev += payload[index]
+
+		print 'Firmware Revision: 0x', format(self.firmwareRev, '08X'), datetime.fromtimestamp(self.firmwareRev)
+
+		# TODO: what else can we pull from revision info payload? Bootloader revision?
+
+	def playSong(self, index):
+		payload = [0xb6, 0x04, index, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 ,0x00, 0x00, 
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+
+		self.usbDev.ctrl_transfer(bmRequestType=0x21, bRequest=9, wValue=0x0300, wIndex=2, 
+			data_or_wLength=payload)
+
 
 def main(argv):
+	songIndex = 0
+
 	try:
 		opts, args = getopt.getopt(argv,"hp:",["playsong="])
 	except getopt.GetoptError:
@@ -26,27 +77,10 @@ def main(argv):
 			print 'SteamControllerConfig.py -p <songIndex>'
 		elif opt in ("-p", "--playsong"):
 			songIndex = int(arg)
-		
-	##TODO: make class that inherits from pyusb instead of using global?
-	global dev
 
-	# find our device
-	dev = usb.core.find(idVendor=0x28DE, idProduct=0x1102)
+	controller = SteamControllerConfig()
 
-	# was it found?
-	if dev is None:
-	    raise ValueError('Wired Steam Controller not found')
-
-	print "Found Wired Steam Controller"
-
-	# TODO: what about reattaching controller on exit?
-	if (dev.is_kernel_driver_active(interface=2)):
-		dev.detach_kernel_driver(interface=2)
-
-	# TODO: need to claim interface?
-	#dev.claim_interface(
-
-	playSong(songIndex)
+	controller.playSong(songIndex)
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
