434ba6a8a0f56577339384775f71d604d3d32764
Gregory Gluszek <ggluszek@gmail.com>
2017-06-02 23:59:13 -0400

    More details added.
    Better organization and correlation back to simulation and firmware I think.
     Based on latest run with updated pinkySim with better log comments.

--- vcf_wired_controller_d0g_57bf5c10.c
+++ vcf_wired_controller_d0g_57bf5c10.c
@@ -2,7 +2,7 @@
 /**
  * In this simulation run the system was run from reset with no external input
  *  (except steps necessary to simulate expected hardware unit reactions). Possible
- *  triggering of IRQs were ignored. Parsed from runLogFile_00000000001494552728.csv.
+ *  triggering of IRQs were ignored. Parsed from exeLog_00000000001496459595.csv.
  *
  * \return None.
  */
@@ -11,9 +11,12 @@ void init()
 	uint32_t* reg = NULL;
 	uint32_t val = 0;
 
-	// Set Power Configuration Register to make sure crystal oscillator is set
-        //  Entry Num: TODO
-        //  Step Num: TODO
+        // Entry Num: 1 - 24
+        // Step Num: 1 - 16
+	// Firmware Offset(s): 
+	//	0x000000d4 - 0x000000d6
+	//	0x00000fe0 - 0x00000fe4 
+	//	0x000005a4 - 0x000005b8
 
 	// Power Configuration Register                                                 
 	reg = 0x40048238;                                                               
@@ -27,36 +30,34 @@ void init()
 	val |= 0xe800;                                                                  
 	*reg = val;  
 
-	// Delay (after Power Configuration Register change I think...)
-        //  Entry Num: TODO
-        //  Step Num: TODO
+        // Entry Num: 25 - 45087
+        // Step Num: 17 - 33814
+	// Firmware Offset(s): 
+	//	0x00000fe8 - 0x00000ffe
 
 	// Some sort of delay required after last system control register mod?          
 	for (uint32_t cnt = 0; cnt < 0x1600; cnt++)                                     
-	{                                                                               
-		nop;                                                                    
-	}  
+	{}  
 
-	// Select Crystal Oscillator
-        //  Entry Num: TODO
-        //  Step Num: TODO
+        // Entry Num: 45088 - 45098
+        // Step Num: 33815 - 33821
+	// Firmware Offset(s): 
+	//	0x00000520 - 0x0000052c
 
 	// Select Crystal Oscillator (SYSOSC)                                           
 	reg = 0x40048040;                                                               
 	*reg = 1;                                                                       
-
-	// Enable system PLL clock source update
-        //  Entry Num: TODO
-        //  Step Num: TODO
 
 	// Enable system PLL clock source update                                        
 	reg = 0x40048044;                                                               
 	*reg = 0;                                                                       
 	*reg = 1;
 
-	// Set PLL divider
-        //  Entry Num: TODO
-        //  Step Num: TODO
+        // Entry Num: 45099 - 45115
+        // Step Num: 33822 - 33834
+	// Firmware Offset(s): 
+	//	0x00001002 - 0x00001004
+	//	0x00000584 - 0x00000598
 
 	// Power Configuration Register                                                 
 	reg = 0x40048238;                                                               
@@ -70,10 +71,20 @@ void init()
 	val |= 0xe800;                                                                  
 	*reg = val;                    
 
+        // Entry Num: 45116 - 45122
+        // Step Num: 33835 - 33839
+	// Firmware Offset(s): 
+	//	0x00001008 - 0x00001010
+
 	// System PLL control register                                                  
 	reg = 0x40048008;                                                               
 	// Division ratio = 2 x 4. Feedback divider value = 3 + 1.                      
 	*reg = 0x23;
+
+        // Entry Num: 45123 - 45137
+        // Step Num: 33840 - 33850
+	// Firmware Offset(s): 
+	//	0x000005a4 - 0x000005b8
 
 	// Power configuration register                                                 
 	reg = 0x40048238;                                                               
@@ -86,18 +97,21 @@ void init()
 	val |= 0xe800;                                                                  
 	*reg = val;
 
-	// Wait until PLL is locked
-        //  Entry Num: TODO
-        //  Step Num: TODO
+        // Entry Num: 45138 - 45143
+        // Step Num: 33851 - 33855
+	// Firmware Offset(s): 
+	//	0x00001014 - 0x0000101e
 
 	// System PLL status register                                                   
 	reg = 0x4004800c;                                                               
 	// Wait until PLL is locked                                                     
 	while(((*reg) & 1) == 0);
 
-	// Set System clock divider
-        //  Entry Num: TODO
-        //  Step Num: TODO
+//TODO
+        // Entry Num: 45144 - 
+        // Step Num: 33856 - 
+	// Firmware Offset(s): 
+	//	
 
 	// System clock divider register                                                
 	reg = 0x40048078;                                                               
@@ -188,6 +202,7 @@ void init()
 	// Check main clock source select register and verify is set to PLL output
         //  Entry Num: 53708 - 53722
         //  Step Num: 40672 - 40684
+	//  Firmware Offset(s): 0x00000452 - 0x0000046a
 
 	reg = 0x40048070;
 	val = *reg;
@@ -196,7 +211,12 @@ void init()
 		// Not sure what this path does... TODO?
 	}
 
-	//TODO: stopping at CSV entry 53723 step 40685 of runLogFile_00000000001494552728.csv
+	// 
+        //  Entry Num: 53723 - 
+        //  Step Num: 40685 - 
+	//  Firmware Offset(s): 0x000004d0 - 
+
+
 	//TODO: Remember to pay attention to branches system does and does not take.
 	//TODO: Keep in mind that system will continue after WFI. Need to walk through this simulation and see about options (how does controller shutdown...?)
 	//		I think what happens is that WFI is called for interrupt to run that sets particular registers. 
