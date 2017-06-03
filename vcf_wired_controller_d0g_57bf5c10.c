ac3132e60ad39ae5ce12ab5c157c15f399cc52f9
Gregory Gluszek <ggluszek@gmail.com>
2017-06-03 14:05:10 -0400

    More details added on firmware simulation.

--- vcf_wired_controller_d0g_57bf5c10.c
+++ vcf_wired_controller_d0g_57bf5c10.c
@@ -107,20 +107,15 @@ void init()
 	// Wait until PLL is locked                                                     
 	while(((*reg) & 1) == 0);
 
-//TODO
-        // Entry Num: 45144 - 
-        // Step Num: 33856 - 
+        // Entry Num: 45144 - 45161
+        // Step Num: 33856 - 33868
 	// Firmware Offset(s): 
-	//	
+	//	0x00001020 - 0x00001038
 
 	// System clock divider register                                                
 	reg = 0x40048078;                                                               
 	// Set system AHB clock divider to 1.                                           
 	*reg = 1;
-
-	// EEPROM flash access configuration
-        //  Entry Num: TODO
-        //  Step Num: TODO
 
 	// Flash configuration register                                                 
 	reg = 0x4003c010;                                                               
@@ -131,9 +126,10 @@ void init()
 	val |=  2;                                                                      
 	*reg = val;
 
-	// Main clock config 
-        //  Entry Num: TODO
-        //  Step Num: TODO
+        // Entry Num: 45162 - 45171
+        // Step Num: 33869 - 33874
+	// Firmware Offset(s): 
+	//	0x0000050c - 0x00000518
 
 	// Main clock source select register                                            
 	reg = 0x40048070;                                                               
@@ -147,9 +143,11 @@ void init()
 	// Update clock source                                                          
 	*reg = 1; 
 
-	// USB configuration
-        //  Entry Num: TODO
-        //  Step Num: TODO
+        // Entry Num: 45172 - 45185
+        // Step Num: 33875 - 33884
+	// Firmware Offset(s): 
+	//	0x0000103c - 0x0000103e
+	//	0x00000548 - 0x00000554
 
 	// USB PLL clock source select register                                         
 	reg = 0x40048048;                                                               
@@ -163,10 +161,20 @@ void init()
 	// Update clock source.                                                         
 	*reg = 1; 
 
+        // Entry Num: 45186 - 45190
+        // Step Num: 33885 - 33888
+	// Firmware Offset(s): 
+	//	0x00001042 - 0x00001048
+
 	// USB PLL control register                                                     
 	reg = 0x40048010;                                                               
 	// Division ration is 2 x 4. Feedback divider value is 3 + 1.                   
 	*reg = 0x23;
+
+        // Entry Num: 45191 - 45205
+        // Step Num: 33889 - 33899
+	// Firmware Offset(s): 
+	//	0x000005a4 - 0x000005b8
 
 	// Power Configuration Register                                                 
 	reg = 0x40048238;                                                               
@@ -179,14 +187,21 @@ void init()
 	val |= 0xe800;                                                                  
 	*reg = val;
 
+        // Entry Num: 45206 - 45211
+        // Step Num: 33900 - 33904
+	// Firmware Offset(s): 
+	//	0x0000104c - 0x0000104c
+	//	0x00001050 - 0x00001056
+
 	// USB PLL status register                                                      
 	reg = 0x40048014;                                                               
 	// Wait for PLL locked                                                          
 	while (((*reg) & 1) == 0);
 
-	// Enable I/O Configuration Block
-        //  Entry Num: TODO
-        //  Step Num: TODO
+        // Entry Num: 45212 - 45226
+        // Step Num: 33905 - 33912
+	// Firmware Offset(s): 
+	//	0x00001058 - 0x00001066
 
 	// System clock control register                                                
 	reg = 0x40048080;                                                               
@@ -195,29 +210,71 @@ void init()
 	val |= 0x10000;                                                                 
 	*reg = val;
 
+        // Entry Num: 45227 - 53704
+        // Step Num: 33913 - 40670 
+	// Firmware Offset(s): 
+	//	0x000000d8 - 0x000000da
+	//	0x000000d0 - 0x000000c4
+	//	0x000003d0 - 0x000003ca
+	//	0x000003bc - 0x000003c4
+	//	0x0000152c - 0x00001536
+	//	0x0000152e - 0x00001538	
+	//	0x000003c6 - 0x000003ca
+	//	0x000003bc - 0x000003c4
+	//	0x0000153c - 0x0000153e
+	//	0x00001540 - 0x00001548
+	//	0x000003c6 - 0x000003cc
+	//	0x000000cc - 0x000000ca
+	//	0x0000154c - 0x0000154e
+	//	0x00000fd0 - 0x00000fd2
+	//	0x00000494 - 0x00000496
+
 	// Set SRAM0 0x10000200 to 0x10001c1c with specific values
-        //  Entry Num: TODO
-        //  Step Num: TODO
+	// TODO: details on exact values set here and data word size accesses
+
+        // Entry Num: 53705 - 53722
+        // Step Num: 40671 - 40684
+	// Firmware Offset(s): 
+	//	0x00000450 - 0x0000046a
 
 	// Check main clock source select register and verify is set to PLL output
-        //  Entry Num: 53708 - 53722
-        //  Step Num: 40672 - 40684
-	//  Firmware Offset(s): 0x00000452 - 0x0000046a
+	//  reg = 0x40048070;
+	//  val = *reg;
+	//  if (val != 0x3)
+	//  {
+		// There are multiple paths for checking other values set here... Have not work through them
+		// TODO: UKNOWN PATHS
+		// 	Not branching to 0x00000470 
+		// 	Not branching to 0x00000474 
+		// 	Not branching to 0x0000047a 
+		// 	Not branching to 0x0000046e 
+	//  }
 
-	reg = 0x40048070;
-	val = *reg;
-	if (val != 0x3)
-	{
-		// Not sure what this path does... TODO?
-	}
+        // Entry Num: 53723 - 53740
+        // Step Num: 40685 - 40696
+	// Firmware Offset(s): 
+	//	0x000004d0 - 0x000004d2
+	//	0x000004a8 - 0x000004b4
+	//	0x000004be - 0x000004c2
 
-	// 
-        //  Entry Num: 53723 - 
-        //  Step Num: 40685 - 
-	//  Firmware Offset(s): 0x000004d0 - 
+	// Check system PLL clock source select register and verify is set to Crystal Oscillator (SYSOSC)
+	//  reg = 0x40048040;
+	//  val = *reg;
+	//  if (val != 1)
+	//  {
+		// There are multiple paths for checking other values set here... Have not work through them
+		// TODO: UKNOWN PATHS
+		// 	Not branching to 0x000004ba
+		// 	Branching to 0x000004be, not executing 0x000004b6
+	//  }
+
+        // Entry Num: 53741 - 
+        // Step Num: 40697 - 
+	// Firmware Offset(s): 
+	//	
 
 
-	//TODO: Remember to pay attention to branches system does and does not take.
+	//TODO: Remember to pay attention to branches/paths simulation does and does not take.
 	//TODO: Keep in mind that system will continue after WFI. Need to walk through this simulation and see about options (how does controller shutdown...?)
 	//		I think what happens is that WFI is called for interrupt to run that sets particular registers. 
 	//		Maybe 32 bit timer interrupting before other things happening causes variables to get set in a way such that power off occurs
